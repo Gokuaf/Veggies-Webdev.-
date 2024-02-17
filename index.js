2f8d5afa29bbdd31476e9966f9e9e11a954f36c4
Gokuaf <ashsh1583@gmail.com>
2024-02-17 14:10:43 +0530

    file edits

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const fs = require("fs");
-const http = require("http");
-const url = require("url");
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
 
 //  Blocking, synchronous way
 //  const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
@@ -30,20 +30,27 @@ const server = http.createServer((req, res) => {
 
   const pathName = req.url;
 
-  if (pathName === "/" || pathName === "/overview") {
-    res.end("this is the overview");
-  } else if (pathName === "/product") {
-    res.end("this is the product");
-  } else{
+  if (pathName === '/' || pathName === '/overview') {
+    res.end('this is the overview');
+  } else if (pathName === '/product') {
+    res.end('this is the product');
+  } else if (pathName === '/api') {
+    fs.readFile(`./dev-data/data.json`, 'utf-8', (err, data) => {
+      const productData = JSON.parse(data);
+      console.log(productData);
+      res.statusCode = 200;
+      res.end('API');
+    });
+  } else {
     res.writeHead(404, {
       'content-type': 'text/html',
-      'my-own-header': 'gelo-world'
+      'my-own-header': 'gelo-world',
     });
     res.end('this page couldnt to be found');
   }
   // res.end("hello from server");
 });
 
-server.listen(8000, "127.0.0.1", () => {
+server.listen(8000, '127.0.0.1', () => {
   console.log(`listening for requests on8000`);
 });
